c8a2b43ba5ffdfd8333e8781bcd889582af21b6b
Heikki Koivula <heikki.koivula@gmail.com>
2021-03-20 15:16:46 +0200

    exercise 2.9 complete

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,10 +3,14 @@ import Person from './components/Person';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
@@ -31,9 +35,32 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  };
+
+  const contentToShow =
+    newFilter === ''
+      ? persons
+      : persons.filter(
+          (person) =>
+            person.name
+              .toLocaleLowerCase()
+              .includes(newFilter.toLocaleLowerCase()) ||
+            person.number.includes(newFilter),
+        );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with:{' '}
+          <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -48,7 +75,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {contentToShow.map((person) => (
           <Person
             key={person.name}
             name={person.name}
